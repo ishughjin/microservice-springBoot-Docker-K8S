c7500230e5790fcae39d64f1cb1a653228a8df6b
Wenxuan Lu <hughjin@Hughs-MacBook-Pro.local>
2024-03-04 13:41:10 -0500

    Create Account API

---
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.eazybytes.accounts.exception;
+
+import com.eazybytes.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+public class GlobalExceptionHandler {
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
